669c5103004bba3003bb642d4add21937e32f99d
guoqiao <guoqiao@gmail.com>
2024-05-25 22:17:10 +1200

    add demo code to show how remote func call other funcs

--- driver/submit.sh
+++ driver/submit.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# working-dir will be zipped as a pkg and upload to ray cluster shm
 ray job submit --address='http://ray_head:8265' --verbose --working-dir . -- python "$@"
--- driver/task.py
+++ driver/task.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python3
 
+import utils
 import ray
-# connect to existing cluster
+# connect to existing cluster, do not create
 ray.init(address='auto')
+
 
 from pprint import pprint
 pprint(ray.nodes())
 
+
+def multiple(x, y):
+    return x * y
+
+
 @ray.remote
-def f(x):
+def task(x):
     import time
+    import os
+    print(os.getcwd())
+    # demo how a remote function call other modules
+    x = utils.add(1, 2)
+    print(f'x is {x}')
+    y = multiple(9, 8)
+    print(f'y is {y}')
     time.sleep(3)
     return x * x
 
-obj_refs = [f.remote(i) for i in range(4)]
-pprint(obj_refs)
+obj_ref = task.remote(3)
+pprint(obj_ref)
 
-result = ray.get(obj_refs)
+result = ray.get(obj_ref)
 print(result)
